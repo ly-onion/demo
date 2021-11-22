c22ff88d784147b6e2add7934ef8069e095b0693
ly <961706734@qq.com>
2021-11-22 15:10:43 +0800

    主要提交

---
+++ src/Modify.java
@@ -0,0 +1,17 @@
+/**
+ * @PROJECT_NAME: testdemo
+ * @DESCRIPTION:
+ * @USER: 洋葱
+ * @DATE: 2021/11/22 15:09
+ */
+public class Modify {
+
+    public void test(){
+        firstModify();
+    }
+
+    public String firstModify(){
+        return "first";
+    }
+
+}
